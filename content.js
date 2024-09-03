b82894b7f1675c5c689a1aca9a9700d91565a3ba
Erons <eronmmer@gmail.com>
2024-09-03 03:23:52 +0100

    fix issue with some elements like emojis and links not being parsed correctly

--- content.js
+++ content.js
@@ -1,11 +1,11 @@
 let isXReplacementActive = true;
 
-chrome.storage.local.get("isXReplacementActive", (result) => {
-	isXReplacementActive =
-		result.isXReplacementActive !== undefined
-			? result.isXReplacementActive
-			: true;
-});
+chrome.storage.local.get(
+	"isXReplacementActive",
+	({ isXReplacementActive: storedValue }) => {
+		isXReplacementActive = storedValue ?? true;
+	}
+);
 
 const REGEX = /\bX\b/gi;
 const REPLACEMENT = "Twitter";
@@ -13,13 +13,14 @@ const TWEET_TEXT_SELECTOR = '[data-testid="tweetText"]';
 
 function replaceContent(element) {
 	if (!isXReplacementActive) return;
-	if (element.nodeType === Node.ELEMENT_NODE) {
-		if (element.childNodes.length) {
-			element.childNodes.forEach((node) => replaceContent(node));
-		} else {
-			element.textContent &&
-				(element.textContent = element.textContent.replace(REGEX, REPLACEMENT));
-		}
+
+	if (element.nodeType === Node.TEXT_NODE) {
+		element.textContent = element.textContent.replace(REGEX, REPLACEMENT);
+	} else if (
+		element.nodeType === Node.ELEMENT_NODE &&
+		element.childNodes.length
+	) {
+		element.childNodes.forEach(replaceContent);
 	}
 }
 
@@ -43,10 +44,11 @@ new MutationObserver((mutations) => {
 	});
 }).observe(document.body, { childList: true, subtree: true });
 
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-	if (request.action === "toggleReplacement") {
-		isXReplacementActive = request.enabled;
+chrome.runtime.onMessage.addListener(({ action, enabled }) => {
+	if (action === "toggleReplacement") {
+		isXReplacementActive = enabled;
 		chrome.storage.local.set({ isXReplacementActive });
+
 		if (isXReplacementActive) {
 			document.querySelectorAll(TWEET_TEXT_SELECTOR).forEach(replaceContent);
 		} else {
